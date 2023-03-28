c13f3057a7604291809c5b2d5ba32656ebd03482
SageStage <113499932+SageStage@users.noreply.github.com>
2023-03-28 19:52:09 +0000

    Deploying to gh-pages from @ SageStage/goit-js-hw-09@f598c457629be42c0f14d58d951df20e75be6061 🚀

---
+++ 01-color-switcher.d6b67ea7.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),r=document.querySelector("button[data-stop]");let a=null;r.setAttribute("disabled",""),e.addEventListener("click",(e=>{e.target.setAttribute("disabled",!0),r.removeAttribute("disabled"),a=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),r.addEventListener("click",(t=>{t.target.setAttribute("disabled",!0),e.removeAttribute("disabled"),clearInterval(a)}));
+//# sourceMappingURL=01-color-switcher.d6b67ea7.js.map
